eb3985ce75b58cc25fc6b08748db7f62b572074c
Chris Kihlstrom <ckihlst@gmail.com>
2017-10-31 17:54:58 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,7 +19,7 @@ def position_taken?(board,index)
     board[index] != " " && board[index] != "" && board[index] != nil
 end
 def valid_move?(board,index)
-  if !position_taken?(board,index)
+  if !position_taken?(board,index) && index.between?(0,8) # true
     true
   else
     false
